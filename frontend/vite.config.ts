1a71b09418c3a5aa258c8591d5aa8032c3f53bcd
Renat Nasyrov <renat.1998.ru@gmail.com>
2025-01-26 16:56:59 +0300

    feat: Добавил алиас

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@App': path.resolve(__dirname, './src/app'),
+      '@Shared': path.resolve(__dirname, './src/shared'),
+      '@Entities': path.resolve(__dirname, './src/entities'),
+      '@Features': path.resolve(__dirname, './src/features'),
+      '@Widgets': path.resolve(__dirname, './src/widgets'),
+      '@Pages': path.resolve(__dirname, './src/pages'),
+    }
+  }
 })
