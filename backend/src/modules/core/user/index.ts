c3b9ce7a21147eb19c79195c8b65ff35b1f81c82
Renat Nasyrov <renat.1998.ru@gmail.com>
2025-08-05 21:34:53 +0300

    feat: Добавил регистрацию

--- backend/src/modules/core/user/index.ts
+++ backend/src/modules/core/user/index.ts
@@ -1,29 +1,58 @@
 import { Hono } from 'hono'
+import { IUser } from '../../../types'
+import { logger } from 'hono/logger'
+import { HTTPException } from 'hono/http-exception'
+import { zValidator } from '@hono/zod-validator'
+import { sign } from 'hono/jwt'
+import { z } from 'zod'
+import dbClientInstance from '../../infrastructure/db'
 
-interface User {
-    name: string,
-    login: string,
-    password_hash: string,
-}
+const user = new Hono()
 
-const app = new Hono()
+user.use(logger())
 
-app.get(
-    '/',
-    (c) => {
-        return c.json({
-            ok: true,
-            message: 'Hello world'
-        })
-    }
-)
-
-
-app.post('/register', async (c) => {
-    return c.json({
-        ok: true,
-        message: 'User registered successfully'
-    })
+const schemeRegister = z.object({
+    username: z.string(),
+    password: z.string(),
+    email: z.string().email(),
 })
 
-export default app
+user.post('/register', zValidator('json', schemeRegister), async (c) => {
+    try {
+        const { username, password, email } = c.req.valid('json')
+
+        const dbClient = await dbClientInstance()
+
+        const userPasswordHash = await Bun.password.hash(password, 'bcrypt')
+
+        if (userPasswordHash === null) {
+            throw new HTTPException(500, { message: 'Internal Server Error' })
+        }
+
+        const user = await dbClient.request<IUser>(`
+            INSERT INTO users (username, email, password_hash) VALUES ('${username}', '${email}', '${userPasswordHash}') RETURNING *
+        `)
+
+        const payload = {
+            username,
+            exp: Math.floor(Date.now() / 1000) + 60 * 60,
+            iat: Math.floor(Date.now() / 1000),
+        }
+
+        const token = await sign(payload, Bun.env.SECRET_KEY || '')
+
+        return c.json({
+            payload,
+            token,
+        })
+    } catch (e) {
+        console.error(e)
+        if (e instanceof Error) {
+            throw new HTTPException(500, { message: e.message })
+        } else {
+            throw new HTTPException(500, { message: 'Internal Server Error' })
+        }
+    }
+})
+
+export default user
